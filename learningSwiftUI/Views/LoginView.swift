fe14b26800ac0d8c754a5cef9ca67f2ab6dcae9d
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-04-25 20:40:58 +0300

    login designed

--- learningSwiftUI/Views/LoginView.swift
+++ learningSwiftUI/Views/LoginView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Deneme")
+        
+        NavigationStack{
+            VStack{
+                //header
+                VStack{
+                    Image("nike")
+                        .resizable()
+                        .frame(width: 250,height: 80)
+                       
+                    
+                    Text("Just Do It")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
+                }.padding(.top, 80)
+                    
+                
+                    
+                Form{
+                    TextField("Email", text: $email)
+                    SecureField("Password", text: $password)
+                }.frame(height: 150)
+                    .padding(.top, 70)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .foregroundStyle(.primary)
+                        Text("Log In")
+                            .foregroundStyle(.white)
+                    }
+                }).frame(height: 50)
+                    .padding(.horizontal)
+                    .padding(.top, 10)
+                    
+                
+                VStack{
+                    NavigationLink("Create new Account", destination: RegisterView())
+                }.padding(.top, 10)
+                Spacer()
+                
+                
+                
+            }
+        }
     }
 }
 
