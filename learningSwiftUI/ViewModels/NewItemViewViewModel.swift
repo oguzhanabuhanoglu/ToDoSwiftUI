93bc4757cb8befbb5b34e78725813cf4584dac4a
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-04-27 15:04:14 +0300

    insert new item to database coded

--- learningSwiftUI/ViewModels/NewItemViewViewModel.swift
+++ learningSwiftUI/ViewModels/NewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject{
     
@@ -17,6 +19,22 @@ class NewItemViewViewModel: ObservableObject{
     
     func save(){
         //save mission to database
+        
+        guard let currentUserId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let id = UUID().uuidString
+        let newItem = ToDoListItem(id: id,
+                                   title: title,
+                                   dueDate: dueDate.timeIntervalSince1970,
+                                   createdDate: Date().timeIntervalSince1970,
+                                   isDone: false)
+        
+        let db = Firestore.firestore()
+        db.collection("User").document(currentUserId).collection("missions").document(id).setData(newItem.asDictionary())
+        
+        
     }
     
     func canSave() -> Bool{
